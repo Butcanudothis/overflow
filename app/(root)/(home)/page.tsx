a42205a4729422738b3272091b920256284e722d
butcanudothis <akshayvarma.varma@gmail.com>
2023-11-08 16:31:04 +0530

    Homepage basic setup

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 text-light-900 ">
+            Ask Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        LocalSearchBar Filters
+      </div>
+    </>
+  );
 };
 
 export default Home;
