6c2577431ce0182756d1a08cb6bf0dd3050db7fa
butcanudothis <akshayvarma.varma@gmail.com>
2023-12-03 09:10:05 +0530

    updated matcher

--- middleware.ts
+++ middleware.ts
@@ -19,5 +19,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
