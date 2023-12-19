a56bf6fbee8a3ba6137dbdade0ec4560923db86f
butcanudothis <akshayvarma.varma@gmail.com>
2023-12-19 22:34:29 +1000

    (feat): localSearch - search state setup with debounce and Url Queries

--- components/shared/search/LocalSearch.tsx
+++ components/shared/search/LocalSearch.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromUrl } from "@/lib/utils";
 
 interface Props {
   route: string;
@@ -18,6 +20,33 @@ const LocalSearch = ({
   placeholder,
   otherClasses,
 }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("q");
+
+  const [search, setSearch] = useState(query || "");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: search,
+        });
+        router.push(newUrl, { scroll: false });
+      } else if (pathname === route) {
+        const newUrl = removeKeysFromUrl({
+          params: searchParams.toString(),
+          keysToRemove: ["q"],
+        });
+        router.push(newUrl, { scroll: false });
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
   return (
     <div
       className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
@@ -34,8 +63,10 @@ const LocalSearch = ({
       <Input
         type="text"
         placeholder={placeholder}
-        value=""
-        onChange={() => {}}
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
         className="paragraph-regular no-focus placeholder background-light800_darkgradient
              border-none shadow-none outline-none"
       />
