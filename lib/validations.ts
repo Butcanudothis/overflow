90d265ba3108b323c01302bb4444a90eac2e7aab
butcanudothis <akshayvarma.varma@gmail.com>
2023-12-19 17:27:03 +1000

    added edit profile form through profile view

--- lib/validations.ts
+++ lib/validations.ts
@@ -17,3 +17,10 @@ export const QuestionSchema = z.object({
 export const AnswerSchema = z.object({
   answer: z.string().min(100, "Answer must be at least 100 characters"),
 });
+export const ProfileEditSchema = z.object({
+  name: z.string().min(5).max(50),
+  username: z.string().min(5).max(50),
+  bio: z.string().min(10).max(150),
+  portfolioLink: z.string().url(),
+  location: z.string().min(5).max(50),
+});
