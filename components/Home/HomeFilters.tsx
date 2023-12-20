41fbc13019e2a1dabca46628d34d0a19658498ce
butcanudothis <akshayvarma.varma@gmail.com>
2023-12-20 13:26:28 +1000

    (feat): filters: home page filters implemented

--- components/Home/HomeFilters.tsx
+++ components/Home/HomeFilters.tsx
@@ -1,21 +1,46 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "@/components/ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "frequent";
+  const searchParams = useSearchParams();
+  const [active, setActive] = useState("");
+  const router = useRouter();
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
         <Button
-          onClick={() => console.log("clicked")}
+          onClick={() => handleTypeClick(item.value)}
           key={item.name}
           className={`body-medium btn rounded-lg px-6 py-3 capitalize shadow-none
           ${
             active === item.value
               ? "bg-primary-100 text-primary-500"
-              : "text-dark300_light700 bg-light-800"
+              : "bg-light-800 text-light-500"
           }`}
         >
           {item.name}
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -21,7 +21,7 @@ export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
     const query: FilterQuery<typeof Question> = {};
     if (searchQuery) {
       query.$or = [
@@ -30,13 +30,30 @@ export async function getQuestions(params: GetQuestionsParams) {
       ];
     }
 
+    let sortOptions = {};
+    switch (filter) {
+      case "newest":
+        sortOptions = { createdAt: -1 };
+        break;
+
+      case "frequent":
+        sortOptions = { views: -1 };
+        break;
+      case "unanswered":
+        query.answers = { $size: 0 };
+        break;
+      default:
+        sortOptions = { createdAt: -1 };
+    }
+
+    // fetch recommended questions
     const questions = await Question.find(query)
       .populate({
         path: "tags",
         model: Tag,
       })
       .populate({ path: "author", model: User })
-      .sort({ createdAt: -1 });
+      .sort(sortOptions);
     console.log(questions);
     return { questions };
   } catch (err) {
