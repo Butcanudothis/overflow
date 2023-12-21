51d999471d61fdeabee668361dc4f155b1dd388d
butcanudothis <akshayvarma.varma@gmail.com>
2023-12-21 21:09:39 +1000

    (feat): pagination: pagination component and setup on home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,11 +9,13 @@ import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
 import { getQuestions } from "@/lib/actions/question.action";
 import { SearchParamsProps } from "@/types";
+import Pagination from "@/components/shared/Pagination";
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const result = await getQuestions({
     searchQuery: searchParams.q,
     filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
   });
   return (
     <>
@@ -64,6 +66,12 @@ export default async function Home({ searchParams }: SearchParamsProps) {
             linkTitle="Ask Question"
           />
         )}
+      </div>
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
       </div>
     </>
   );
